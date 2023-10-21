f311742d60c41fcd8ab7042ff1fac500022f9d51
abhisheksharma <abhisheksharma131098@gmail.com>
2023-10-21 21:08:34 +0530

    achievements added in achievement section

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -1,26 +1,36 @@
 import React from "react";
+import achievements from "../data/achievements";
+
+const AchievementItem = ({ img, text1 = "", text2 = "", text3 = "" }) => {
+  return (
+    <article className="list flex-row justify-start">
+      <div className="icon-wrapper flex-col flex-center">
+        <img className="list-image" src={img} alt="as" />
+      </div>
+      <div className="desc1">
+        <h3>{text1}</h3>
+        <h4>{text2}</h4>
+        <p>{text3}</p>
+      </div>
+    </article>
+  );
+};
 
 const Achievements = () => {
-  const AchievementItem = () => {
-    return (
-      <article className="list flex-row justify-center">
-        <img className="list-image" src="" alt="as" />
-        <div className="desc1">
-          <h3>UPWORK </h3>
-          <h4>Freelance</h4>
-          <p>2021-2023</p>
-        </div>
-      </article>
-    );
-  };
-
   return (
-    <section id="achievements" className="flex-col">
+    <section id="achievements" className="flex-col flex-center">
       <div className="s-head heading">
         <h2>Achievements</h2>
       </div>
-      <div className="flex-col flex-center">
-        <AchievementItem />
+      <div className="flex-col flex-center gap-1">
+        {achievements?.map((ach) => (
+          <AchievementItem
+            img={ach?.img}
+            text1={ach?.title}
+            text2={ach?.description}
+            text3={ach?.date}
+          />
+        ))}
       </div>
     </section>
   );
---
+++ src/data/achievements.js
@@ -0,0 +1,47 @@
+import metaBadge from "../assets/images/meta-front-end.png";
+import fossAsia from "../assets/images/foss-asia.jpg";
+import microsoft from "../assets/images/microsoft.png";
+import google from "../assets/images/google.png";
+import trophy from "../assets/icons/trophy.svg";
+import secondTrophy from "../assets/images/second-trophy.png";
+
+const achievements = [
+  {
+    title: "Meta Front-End Developer Specialization Badge",
+    description: "Meta through Coursera",
+    date: "Oct 2023",
+    img: metaBadge,
+  },
+  {
+    title: "Digital swag from Microsoft",
+    description: "FOSSASIA Summit 2022 Cloud Skills Challenge",
+    date: "April 2022",
+    img: fossAsia,
+  },
+  {
+    title: "Badges and Trophies from Microsoft",
+    description: "Microsoft Learn Platform for learning Azure and AI",
+    date: "Sep 2020 - Dec 2020",
+    img: microsoft,
+  },
+  {
+    title: "Cloud Hero Badge from Google",
+    description: "Google Cloud",
+    date: "Nov 2020",
+    img: google,
+  },
+  {
+    title: "Winner of Hackathon 2019",
+    description: "Haryana Engineering College, Yamunanagar, Haryana",
+    date: "April 2019",
+    img: trophy,
+  },
+  {
+    title: "1St Runner-Up of Arduino Day 2019 Competition",
+    description: "Teerthanker Mahaveer University, Moradabad, U.P",
+    date: "March 2019",
+    img: secondTrophy,
+  },
+];
+
+export default achievements;
