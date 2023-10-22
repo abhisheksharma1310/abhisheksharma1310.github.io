f7785b4c7d2b077ba0e7566d7ee8445b573637ec
abhisheksharma <abhisheksharma131098@gmail.com>
2023-10-22 12:51:44 +0530

    information added in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,24 +1,19 @@
 import React from "react";
+import about from "../data/about";
 
 const About = () => {
   return (
-    <section id="about" className="flex-col">
+    <section id="about" className="flex-col flex-center">
       <div className="s-head heading">
         <h2>About</h2>
       </div>
-      <article className="list flex-row justify-center">
-        <img className="list-image" src="" alt="as" width="500px"/>
-        <div className="desc1 flex-col">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <a className="btn-link" href="" target="_blank">
+      <article className="flex-col flex-center">
+        <img className="about-img" src={about?.img} alt="as" width="500px"/>
+        <div className="desc1 flex-col flex-start gap-1">
+          <>
+            {about?.about.split("##").map((text) => text && <p>{text}</p>)}
+          </>
+          <a className="btn-link" href={about?.resume} target="_blank" rel="noreferrer">
             View Resume
           </a>
         </div>
---
+++ src/data/about.js
@@ -0,0 +1,16 @@
+import image from "../assets/images/about@3x.webp";
+const exp = new Date().getFullYear() - 2022;
+
+const about = {
+  img: image,
+  about: `Abhishek Sharma here, a Full-stack Web Developer boasting over ${exp} ${exp > 1 ? "years" : "year"} of experience in both Front-End and Back-End application development, encompassing testing, debugging, and deployment. Proficient in Reactjs, Nextjs, and Nodejs, I possess a deep understanding of the web development lifecycle process. JavaScript holds a special place in my heart, and I've happily utilized it across various projects. My work has spanned a range of JavaScript libraries and frameworks, including ReactJs, NextJs, NodeJs, and ExpressJs.
+    ##
+  In addition, I've had the opportunity to work with state management libraries such as Redux, Redux Toolkit, and Mobex State Tree. When it comes to CSS and UI libraries, I'm well-versed in BootStrap, Tailwind CSS, Material UI, and React Suite. Furthermore, I've integrated various other libraries like React-Router-Dom, Formik, React-Form, and ChartJs into my projects. My experience extends to utilizing Browser Web APIs and Services, such as Local Storage, Session Storage, and Progressive Web App (PWA) API.
+    ##
+  I've had the privilege of harnessing the power of cloud services, including Firebase, GCP, AWS, and Azure. My journey in the tech world has also included tinkering with IoT devices like Arduino, Raspberry Pi, and Esp32. With over ten small projects and five large projects under my belt, I've honed my skills and understanding of UI/UX concepts and design tools like Figma. I'm well-versed in ensuring web app responsiveness, cross-device, and cross-browser compatibility, as well as accessibility for all users.
+    ##
+  Version control systems like Git, along with platforms like GitHub and Bitbucket, have been instrumental in my workflow. Additionally, I've had the privilege of creating CI/CD pipelines. Currently, I'm actively seeking new opportunities in the field.`,
+  resume: "",
+};
+
+export default about;
